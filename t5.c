17e956ada1d301e44f5d0053e29cf90dfe1f390b
haseeflafir17-alt <haseeflafir17@gmail.com>
2026-05-15 23:37:26 +0530

    1st

---
+++ t5.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(){
+    int n,k=0;
+    printf("Enter the number: ");
+    scanf("%d",&n);
+    for(int i = 1;i<=n;i++){
+        for(int j=i;j<=2*n-i;j++){
+            if((j==i)||(j==2*n-i)){
+                printf("%d",i);
+            }
+            else{
+                printf(" ");
+            }
+        }
+        k++;
+        printf("\n");
+        for(int l=0;l<k;l++){
+            printf(" ");
+        }
+    }
+
+    return 0;
+}
